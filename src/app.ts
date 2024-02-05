16e4dbf184208f16130f24bddbfc56696e761d46
Nikita Klyuchnikov <klyuch1994@mail.ru>
2024-02-05 15:17:06 +0500

    сделал 11 пункт

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import routes from './routes/index'
 import { ERROR_CODE_NOT_FOUND } from './errors/errors';
 import { Login, createUser } from './controllers/user';
 import auth from './middlewares/auth';
-
+import  logger from './middlewares/logger';
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(logger.requestLogger);
 
 app.post('/signin', Login);
 app.post('/signup', createUser);
@@ -24,6 +26,8 @@ app.use('/', routes);
 app.use((req: Request, res: Response) => {
   res.status(ERROR_CODE_NOT_FOUND).json({ message: 'Передан несуществующий маршрут' });
 });
+
+app.use(logger.errorLogger);
 
 
 
